3abbce1438e7f77c6b463b87f1065754ae65ff6f
Sprynimble <70118635+5pryn@users.noreply.github.com>
2021-12-10 19:51:03 +0800

    Add files via upload

---
+++ toy-app1_v1.0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 19:01:25 2021
+
+@author: steph
+"""
+
+import streamlit as st
+# other libs
+import numpy as np
+import pandas as pd
+
+st.title('Toy App-1 on the go..')
+st.balloons() 
